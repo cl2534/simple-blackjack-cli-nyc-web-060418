8a4daeb2aca043df73bd3ff7a3bfa34e5dc222ce
Changmin Lim <chang.lim92@gmail.com>
2018-05-25 05:32:56 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -27,10 +27,8 @@ end
 
 def initial_round
   # code #initial_round here
-  2.times do 
-    deal_card()
-  end
-  display_card_total()
+  sum = deal_card() + deal_card()
+  display_card_total(sum)
 end
 
 def hit?
